b354494fc9d1c7c420cfe16071bbe815ca17da11
Durga Reddy <durgareddy677@gmail.com>
2018-02-10 15:04:08 +0530

    test

--- src/login/login.controller.js
+++ src/login/login.controller.js
@@ -1,14 +1,32 @@
 class LoginController {
-    constructor()
+    constructor($http)
     {
-     
+      this.$http=$http;
       this.email="";
       this.password="";
+      this.error="";
     }
     doLogin(){
        
-      console.log(this.email);
-      console.log(this.password);
+      var body={
+              "email":this.email,
+              "password":this.password
+
+            };
+            this.$http.post("http://localhost:3000/api/user/login",body)
+            .then((success)=>{
+                  //server responded with success
+                  alert("success");
+            },(error)=>{
+              //server responded with error
+             this.error=error.data.error;
+            })
+            .catch((exception)=>{
+              //network exception 
+              console.error(exception);
+            })
+
     }
 }
+LoginController.$inject=["$http"];
 export default LoginController;
